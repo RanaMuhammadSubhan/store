75370b9b77103748b9bd829d603b358df7e4210f
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-11-06 15:51:12 +0500

    Small Changes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,33 +4,33 @@ const Category = require('../models/Category');
 // const multer = require('multer');
 
 // Define storage for uploaded files
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'uploads/'); // Specify the directory where uploaded files will be stored
-  },
-  filename: (req, file, callback) => {
-    // Replace backslashes with forward slashes in the file path
-    const webPath = file.originalname.replace(/\\/g, '/');
-    callback(null, webPath);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'uploads/'); // Specify the directory where uploaded files will be stored
+//   },
+//   filename: (req, file, callback) => {
+//     // Replace backslashes with forward slashes in the file path
+//     const webPath = file.originalname.replace(/\\/g, '/');
+//     callback(null, webPath);
+//   },
+// });
 
 
 // const upload = multer({ storage: storage });
 
 // Route to create a new category with image upload
-router.post('/categories', upload.single('image'), async (req, res) => {
-  try {
-    const { name } = req.body;
-    let image = req.file ? req.file.path : 'image is not uploaded';
+// router.post('/categories', upload.single('image'), async (req, res) => {
+//   try {
+//     const { name } = req.body;
+//     let image = req.file ? req.file.path : 'image is not uploaded';
     
-    // Replace backslashes with forward slashes in the image path
-    image = image.replace(/\\/g, '/');
+//     // Replace backslashes with forward slashes in the image path
+//     image = image.replace(/\\/g, '/');
 
-    const newCategory = new Category({
-      name,
-      image,
-    });
+//     const newCategory = new Category({
+//       name,
+//       image,
+//     });
 
 //     await newCategory.save();
 //     res.json(newCategory);
